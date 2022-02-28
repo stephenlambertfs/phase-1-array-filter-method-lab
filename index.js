d7b737963014872ece8a15310f37e9db5fe29910
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-02-28 10:13:58 -0700

    commit tree 413d4d928ca75571376def0814ae068d170f7c77

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+function findMatching(name,string){
+    let result = name.filter(word => word === string);
+    if(name.find(word => word == string) != string){
+        return result;
+    }
+    return result;
+}
